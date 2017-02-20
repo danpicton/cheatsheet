8d8d42236e7e9d1ddfece1ac0ca50e4d16d11fca
dan <daniel.picton@gmail.com>
2017-02-20 22:01:25 +0000

    Added basic QWebView code
    Created a QWebView class, loaded Google homepage into it and made
    it transparent. Instantiated the class successfully, so will need
    to pass in custom HTML next.

--- chsh.py
+++ chsh.py
@@ -1,18 +1,21 @@
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui, QtCore, QtWebKit
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+from PyQt4.QtWebKit import QWebView
+#from PyQt4.QtGui import QApplication
+#from PyQt4.QtCore import QUrl
 
 class mymainwindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
-        wid = QtGui.QWidget(self)
-        grid = QtGui.QGridLayout()
-        self.setCentralWidget(wid)
-        for i in range(1,5):
-            for j in range(1,5):
-                grid.addWidget(QtGui.QLabel("test"+str(i)+","+str(j)))
-        wid.setLayout(grid)
+        #wid = QtGui.QWidget(self)
+        #grid = QtGui.QGridLayout()
+        #self.setCentralWidget(wid)
+        #for i in range(1,5):
+        #    for j in range(1,5):
+        #        grid.addWidget(QtGui.QLabel("test"+str(i)+","+str(j)))
+        #wid.setLayout(grid)
         self.setWindowFlags(
             QtCore.Qt.WindowStaysOnTopHint |
             QtCore.Qt.FramelessWindowHint |
@@ -25,13 +28,21 @@ class mymainwindow(QtGui.QMainWindow):
         )
         self.setWindowOpacity(0.9)
         self.setStyleSheet('QMainWindow{background-color: darkgray; border: 5px solid darkgray}')
-        #self.label = QtGui.QLabel(self)
-        #self.label.setText('cats')
+        self.label = QtGui.QLabel(self)
+        self.label.setText('cats')
 
     def mousePressEvent(self, event):
         QtGui.qApp.quit()
 
+class myWebView(QtWebKit.QWebView):
+    def __init__(self):
+        QtWebKit.QWebView.__init__(self)
+        self.load(QUrl("https://www.google.co.uk/"))
+        self.setWindowOpacity(0.5)
+
 app = QtGui.QApplication(sys.argv)
-mywindow = mymainwindow()
-mywindow.show()
+browser = myWebView()
+browser.show()
+#mywindow = mymainwindow()
+#mywindow.show()
 app.exec_()
